d64361bf6d2cb615cf735596bc5057d5c0bb7d98
Jianfeng Zhang <swordfeng123@gmail.com>
2016-06-17 00:15:26 +0800

    update

--- src/Main.hs
+++ src/Main.hs
@@ -12,9 +12,10 @@ import Network.HTTP.Client (Manager)
 import Servant.Common.Req (ServantError)
 import qualified Text.Regex as Regex
 import qualified Data.Text as T
-import Control.Exception (try, SomeException)
+import Control.Exception (try, SomeException, displayException)
 import Control.Monad (guard)
 import Secret
+import qualified Data.List as L
 
 main = do
     manager <- newManager tlsManagerSettings
@@ -30,7 +31,8 @@ processUpdates :: Token -> Manager -> Maybe Int -> IO ()
 processUpdates token manager startPos = do
     result <- getUpdates token startPos Nothing (Just 30) manager
     case result of
-        Left e -> print e >> processUpdates token manager startPos
+        Left e -> (if L.isInfixOf "ResponseTimeout" (displayException e) then return () else print e)
+            >> processUpdates token manager startPos
         Right UpdatesResponse { update_result = updates } -> do
             forM_ updates handleUpdate
             processUpdates token manager (Just . (+1) . maximum . map update_id $ updates)
@@ -39,17 +41,41 @@ processUpdates token manager startPos = do
             forM_ textMessageHandlers $ \handler -> try (handler msg) :: IO (Either SomeException ())
         handleUpdate _ = return ()
         textMessageHandlers = [pingHandler, startHandler]
-        pingRegex = Regex.mkRegex "^/ping(@swbiobot|)\\b"
+        pingRegex = Regex.mkRegex "^/help(@swbiobot|)\\b"
         pingHandler msg@(Message { text = Just str }) = do
             guard $ Regex.matchRegex pingRegex (T.unpack str) /= Nothing
-            let req = sendMessageRequest (T.pack . show . chat_id . chat $ msg) "pong"
+            let req = messageReq {
+                message_chat_id = T.pack . show . chat_id . chat $ msg,
+                message_text = helpMessage,
+                message_reply_to_message_id = Just $ message_id msg
+            }
             result <- sendMessage token req manager
             case result of
                 Left e -> print e
         startRegex = Regex.mkRegex "^/start(@swbiobot|)\\b"
         startHandler msg@(Message { text = Just str }) = do
             guard $ Regex.matchRegex startRegex (T.unpack str) /= Nothing
-            let req = sendMessageRequest (T.pack . show . chat_id . chat $ msg) "Welcome to use BioBot by @swordfeng!"
+            let req = messageReq {
+                message_chat_id = (T.pack . show . chat_id . chat $ msg),
+                message_text = helpMessage,
+                message_reply_to_message_id = Just $ message_id msg
+            }
             result <- sendMessage token req manager
             case result of
                 Left e -> print e
+
+helpMessage = T.pack $ L.intercalate "\n" [
+    "Welcome to BioBot by @swordfeng!",
+    "Commands:",
+    "    /help - show this help"
+    ]
+
+messageReq = SendMessageRequest {
+    message_chat_id = T.pack "",
+    message_text = "",
+    message_parse_mode = Just Markdown,
+    message_disable_web_page_preview = Nothing,
+    message_disable_notification = Nothing,
+    message_reply_to_message_id = Nothing,
+    message_reply_markup = Nothing
+}
